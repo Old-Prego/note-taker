b387af5859e2a84b49d5de3a1ca1751f6f50844f
Old Prego <spymasterluke@gmail.com>
2021-04-25 16:29:22 -0500

    Worked out the routing and now working on the API calls

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const path = require('path');
+const http = require('http');
+const fs = require('fs');
+
+const app = express();
+app.use(express.static('public'));
+const PORT = process.env.PORT || 3000;
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
+app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
+
+app.get('/api/notes', (req,res) => res.json(__dirname + '/db/db.json'))
+
+app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
