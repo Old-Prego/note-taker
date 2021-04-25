44312820dacec8c47552cb2905ec11b6e06c2e39
Old Prego <spymasterluke@gmail.com>
2021-04-25 17:51:31 -0500

    Got the posting with ID's and loading up previous notes working properly now. Just need to figure out why it isn't refreshing after the POST

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const fs = require('fs');
-const {v4: uuidv4 } = require('uuid');
+const uniqid = require('uniqid');
 
 const app = express();
 app.use(express.static('public'));
@@ -14,13 +14,19 @@ app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html'
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
 app.get('/api/notes', (req, res) => {
-    res.send(__dirname + '/db/db.json')
-})
+    fs.readFile(__dirname + '/db/db.json', function (err, data) {
+        var json = JSON.parse(data);
+        res.json(json);
+    }
+)});
+
+app.get('/api/notes/:note')
 
 app.post('/api/notes', (req, res) => {
 
     fs.readFile(__dirname + '/db/db.json', function (err, data) {
         var json = JSON.parse(data);
+        req.body.id = uniqid();
         json.push(req.body);
         console.log(json);
         fs.writeFile(__dirname + '/db/db.json', JSON.stringify(json), function(err){
