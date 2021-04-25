42b44639fed8b9da4d648643b51e635b7a31c098
Old Prego <spymasterluke@gmail.com>
2021-04-25 17:18:04 -0500

    Got the posting to work! Yay! Now on to making the get method working properly. Can't figure out why it is being so difficult

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const path = require('path');
 const http = require('http');
 const fs = require('fs');
+const {v4: uuidv4 } = require('uuid');
 
 const app = express();
 app.use(express.static('public'));
@@ -12,6 +13,22 @@ app.use(express.json());
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'public/index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(__dirname, 'public/notes.html')));
 
-app.get('/api/notes', (req,res) => res.json(__dirname + '/db/db.json'))
+app.get('/api/notes', (req, res) => {
+    res.send(__dirname + '/db/db.json')
+})
+
+app.post('/api/notes', (req, res) => {
+
+    fs.readFile(__dirname + '/db/db.json', function (err, data) {
+        var json = JSON.parse(data);
+        json.push(req.body);
+        console.log(json);
+        fs.writeFile(__dirname + '/db/db.json', JSON.stringify(json), function(err){
+            if (err) throw err;
+            console.log('Success!');
+        })
+    })
+
+})
 
 app.listen(PORT, () => console.log(`App listening on PORT ${PORT}`));
